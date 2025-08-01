1b31c114f71b80151b1427180c0b389e9340ce31
elanuk <119196802+elnukk@users.noreply.github.com>
2025-08-01 01:24:44 -0700

    districts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
   let error = null;
   let csvData = "";
 
-  const districts = ["Patna", "Gaya", "Nalanda", "Siwan", "Bhagalpur"];
+  const districts = ["Arwal", "Aurangabad", "Banka", "Begusarai", "Bhagalpur", "Bhojpur", "Buxar", "Darbhanga", "East Champaran", "Gaya", "Gopalganj","Jamui","Jehanabad","Kaimur","Katihar","Khagaria","Kishanganj","Lakhisarai","Madhepura","Madhubani","Munger","Muzaffarpur","Nalanda","Nawada","Patna","Purnia","Rohtas","Saharsa","Samastipur","Saran","Sheikhpura","Sheohar","Sitamarhi","Siwan","Supaul", "Vaishali","West Champaran"];
 
   async function submit() {
     if (!district) return;
